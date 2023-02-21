4f119d4918440b6e695ee1353f701ab929e63b90
Soufian Tamim <soufiaintamim22@gmail.com>
2023-02-21 16:25:51 +0100

    initial commit

--- php/index.php
+++ php/index.php
@@ -1,63 +1,28 @@
 <?php 
 require 'connect.php';
 
-// if(isset($_POST['filtrer'])){//check if the button exists  in the form 
+if(isset($_POST['filtrer'])){//check if the button exists  in the form 
 
+  $Date = $_POST["Date"];
+  $MinPrice = $_POST["MinPrice"];
+  $MaxPrice = $_POST["MaxPrice"];
 
-//   $MinPrice = $_POST["MinPrice"];
-//   $MaxPrice = $_POST["MaxPrice"];
-//   //store the request in a variable 
-//   $sql = "SELECT * FROM `announce` WHERE `PRICE` >= $MinPrice AND `PRICE` <= $MaxPrice AND `TYPE` LIKE '$TYPE'";
-//   $result = $conn->query($sql);//execute the request
+  //store the request in a variable 
+  $sql = "SELECT * FROM `announce` WHERE `PRICE` >= $MinPrice AND `PRICE` <= $MaxPrice AND `TYPE` LIKE '$TYPE'";
+  $result = $conn->query($sql);//execute the request
+  $FilterResult = $result->fetchAll(PDO::FETCH_ASSOC);//fetch the filtered result as associative 
 
-//   $FilterResult = $result->fetchAll(PDO::FETCH_ASSOC);//fetch the filtered result as associative 
+} elseif(isset($_POST['Search'])){
+  $Category = $_POST["Category"];
+  $Price = $_POST["Price"];
+  $TYPE = $_POST["Type"];
+  $City = $_POST["City"];
+  $Value = $_POST["Value"];
 
-
-// } elseif(isset($_POST['Search'])){
-
-
-//   $Category = $_POST["Category"];
-//   $Price = $_POST["Price"];
-//   $TYPE = $_POST["Type"];
-//   $City = $_POST["City"];
-//   $Value = $_POST["Value"];
-
-
-//   $sql = "SELECT * FROM `annonce` WHERE `City`='$Value' or `Category`='$Value' or `Type`='$Value' or `Price`=$Value;"
-
-
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  $sqli = "SELECT * FROM `annonce` WHERE `City`='$Value' or `Category`='$Value' or `Type`='$Value' or `Price`=$Value;"
+  $Result = $conn->query($sqli);//execute the request
+  $FilterResult = $Result->fetchAll(PDO::FETCH_ASSOC);//fetch the filtered result as associative 
+}
 
 // else {
 
@@ -79,9 +44,6 @@ require 'connect.php';
 // // execute a query
 // $annoncesLength = $conn->query($sql)->rowCount();
 
-
-
-
 // // echo $annoncesLength[0];
 
 // $pagesNum = 0;
@@ -94,62 +56,7 @@ require 'connect.php';
 //   $pagesNum = ceil($annoncesLength / 8);
 // }
 // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -189,56 +96,18 @@ require 'connect.php';
 
 
 
-
-
-
-
-  <?php
-  if($_SERVER["REQUEST_METHOD"] == "GET"){
-  ?>
-
+  <?php if($_SERVER["REQUEST_METHOD"] == "GET"){?>
   <nav class="mt-4 mb-4 " aria-label="Page navigation example">
     <ul class=" flex-wrap pagination justify-content-center">
       <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-  <?php
-  for ($i=1; $i <= $pagesNum; $i++) { 
-    ?>
+      <?php for ($i=1; $i <= $pagesNum; $i++) { ?>
       <li class="page-item"><a class="page-link" href="<?php echo "index.php?pageId=".$i?>"><?php echo $i ;?></a></li>
-
-  <?php
-
-  }
-
-  ?>
+      <?php}?>
       <li class="page-item"><a class="page-link" href="">Next</a></li>
     </ul>
   </nav>
-  <?php
-
-  }
-
-  ?>
+  <?php}?>
   </div>
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     <script src="js/script.js"></script>
